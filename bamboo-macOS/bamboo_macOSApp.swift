c4693973b82c9589e47bc8448c4f0ced1c9fbce8
kimsh153 <sunghun4777@gmail.com>
2021-10-21 22:53:49 +0900

    Initial Commit

---
+++ bamboo-macOS/bamboo_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  bamboo_macOSApp.swift
+//  bamboo-macOS
+//
+//  Created by gsm10 on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct bamboo_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
